98254903072fb31e4125156a6ad855b77e196342
Ryan Lane <rlane32@gmail.com>
2016-05-20 12:23:37 -0700

    Echo tag info (#17)
    * Echo tag info
    
    * Limit push to specific branches
    
    * Fix bash conditional

--- docker_push.sh
+++ docker_push.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 export REPO=lyft/metadataproxy
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+want_push='false'
+for branch in "master" "1.0" "1.1"
+do
+   if [ "$TRAVIS_BRANCH" == "$branch" ]
+   then
+       want_push='true'
+   fi
+done
+if [ "$TRAVIS_PULL_REQUEST" == "false" -a "$want_push" == "true" ]
 then
     docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-    docker build -f Dockerfile -t $REPO:$COMMIT .
+    echo "TAG is $TAG"
+    docker build -f Dockerfile .
     docker tag $REPO:$COMMIT $REPO:$TAG
-    docker push $REPO
+    docker push $REPO:$TAG
 else
     echo 'Ignoring PR branch for docker push.'
 fi
