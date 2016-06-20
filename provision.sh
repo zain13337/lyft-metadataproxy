02d6c7c8fc925837e4b631abd2f0c543f4cbbaa6
Matt Leventi <mleventi@lyft.com>
2016-06-20 10:25:49 -0700

    creating ip tables rule

--- provision.sh
+++ provision.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
-exec /sbin/iptables --wait -t nat \
-                    -A PREROUTING \
-                    -i docker0 \
-                    -p tcp \
-                    --dport 80 \
-                    --destination 169.254.169.254 \
-                    --jump DNAT \
-                    --to-destination 172.17.0.1:45001
+if ! iptables --wait -t nat -i docker0 -p tcp --dport 80 --destination 169.254.169.254 --jump DNAT --to-destination 172.17.0.1:45001 -C PREROUTING; then
+  echo "creating ip tables rule ..."
+  /sbin/iptables --wait -t nat \
+      -A PREROUTING \
+      -i docker0 \
+      -p tcp \
+      --dport 80 \
+      --destination 169.254.169.254 \
+      --jump DNAT \
+      --to-destination 172.17.0.1:45001
+fi
