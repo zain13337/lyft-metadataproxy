84eb4ec693c602c716c5d16c15b7397f9aff36c2
Ryan Lane <rlane@lyft.com>
2016-06-22 13:21:27 -0700

    Actually bump version to 1.1.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ reqs = reqs + [str(ir.req) for ir in reqs_wsgi]
 
 setup(
     name="metadataproxy",
-    version="1.1.2",
+    version="1.1.3",
     packages=find_packages(exclude=["test*"]),
     install_requires=reqs,
     author="Ryan Lane",
