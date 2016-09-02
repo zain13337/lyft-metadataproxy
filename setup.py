3e25f4f9ca0cdddd9f909809b47a106bb663c4e3
Ryan Lane <rlane32@gmail.com>
2016-09-02 13:29:00 -0700

    Upgrade docker py to avoid auth bug in docker config (#23)

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ reqs = reqs + [str(ir.req) for ir in reqs_wsgi]
 
 setup(
     name="metadataproxy",
-    version="1.1.3",
+    version="1.1.4",
     packages=find_packages(exclude=["test*"]),
     install_requires=reqs,
     author="Ryan Lane",
