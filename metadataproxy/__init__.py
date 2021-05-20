a9931aa9f2d1c72d500db12a4e0182d39218c226
Ryan Lane <ryan.lane@paypay-corp.co.jp>
2021-05-20 18:33:46 +0900

    Append in env

--- metadataproxy/__init__.py
+++ metadataproxy/__init__.py
@@ -8,7 +8,7 @@ app.debug = app.config['DEBUG']
 
 if app.config['PATCH_ECS_ALLOWED_HOSTS']:
     from botocore.utils import ContainerMetadataFetcher  # NOQA
-    ContainerMetadataFetcher._ALLOWED_HOSTS.append('host.docker.internal')
+    ContainerMetadataFetcher._ALLOWED_HOSTS.append(app.config['PATCH_ECS_ALLOWED_HOSTS'])
 
 if app.config['MOCK_API']:
     from metadataproxy.routes import mock  # NOQA
