ea4edc11bea16df39b566a717f6fcb3961b4d045
Ryan Lane <ryan.lane@paypay-corp.co.jp>
2021-05-20 18:20:43 +0900

    Add an option to patch boto to support ecs-server mode of aws-vault

--- metadataproxy/__init__.py
+++ metadataproxy/__init__.py
@@ -2,9 +2,14 @@ from flask import Flask
 
 from metadataproxy import settings
 
+
 app = Flask(__name__)
 app.config.from_object(settings)
 app.debug = app.config['DEBUG']
+
+if app.config['PATCH_ECS_ALLOWED_HOSTS']:
+    from botocore.utils import ContainerMetadataFetcher  # NOQA
+    ContainerMetadataFetcher._ALLOWED_HOSTS.append('host.docker.internal')
 
 if app.config['MOCK_API']:
     from metadataproxy.routes import mock  # NOQA
